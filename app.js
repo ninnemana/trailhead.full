00828c75f1845169f23ab73620ea6d4cdd375b02
Alex Ninneman <ninnemana@gmail.com>
2012-02-17 17:08:28 -0600

    fixed CloudFoundry port

--- app.js
+++ app.js
@@ -33,5 +33,5 @@ app.configure('production', function(){
 
 app.get('/', routes.index);
 
-app.listen(3000);
+app.listen(process.env.VCAP_APP_PORT || 3000);
 console.log("Express server listening on port %d in %s mode", app.address().port, app.settings.env);
